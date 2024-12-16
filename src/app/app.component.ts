d9c5d866930b49faaa57024c30160fa35dd8e70c
hernanfloresramirez <hernan.flores@shipedge.com>
2024-12-15 23:25:14 -0400

    Add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, ButtonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'avitem-frontend';
+
+//   toggleDarkMode() {
+//     const element = document.querySelector('html');
+//     element.classList.toggle('my-app-dark');
+// }
 }
