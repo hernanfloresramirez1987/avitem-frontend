4c188e60303fe674855c46c079e0512350a9be9f
hernanfloresramirez <hernan.flores@shipedge.com>
2024-12-15 23:22:41 -0400

    segundo commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,47 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeng/themes/aura';
+
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json')
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    importProvidersFrom(
+      CommonModule, 
+      BrowserAnimationsModule, 
+      TranslateModule.forRoot({
+        loader: {
+          deps: [HttpClient],
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+        },
+        defaultLanguage: 'es',
+      })
+    ),
+    provideHttpClient(),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    providePrimeNG({ 
+        theme: {
+            preset: Aura
+        }
+    })
+]
 };
