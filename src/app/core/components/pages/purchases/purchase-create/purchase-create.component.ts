bdffc43c3ab3c6c14e2372eede153f90fe7a811b
hernanfloresramirez <hernan.flores@shipedge.com>
2024-12-26 19:09:18 -0400

    Add generation purchases

---
+++ src/app/core/components/pages/purchases/purchase-create/purchase-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './purchase-create.component.html',
+  styleUrl: './purchase-create.component.scss'
+})
+export class PurchaseCreateComponent {
+
+}
