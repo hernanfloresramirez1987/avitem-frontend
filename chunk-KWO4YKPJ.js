4465e6c3772c0bbe7be82c9168142299cdc8aef6
hernanfloresramirez1987 <hernanfloresramirez1987@users.noreply.github.com>
2024-12-30 03:58:49 +0000

    Deploying to gh-pages from @ hernanfloresramirez1987/avitem-frontend@aba1c15325a3f7a1970616ccec78a30f2dcce2d0 🚀

---
+++ chunk-KWO4YKPJ.js
@@ -0,0 +1,1 @@
+import{Lb as r,Tb as s,ea as o,tb as a,ub as n}from"./chunk-ILSE7TFP.js";var m=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=o({type:e,selectors:[["app-dashboard"]],standalone:!0,features:[s],decls:2,vars:0,template:function(t,d){t&1&&(a(0,"p"),r(1,"dashboard works!"),n())}})}}return e})();export{m as default};
