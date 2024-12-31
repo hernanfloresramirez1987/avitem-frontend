a2739f8aa50f5a7779b74c0dd2cf45d85b5349e5
hernanfloresramirez <hernan.flores@shipedge.com>
2024-12-30 22:52:16 -0400

    Add proveedores rutes & view list y create

---
+++ src/app/core/_services/common/user.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+
+  readonly #url= environment.URL_API;
+
+  constructor(private http: HttpClient) { }
+
+}
