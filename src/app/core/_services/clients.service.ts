9ccc21493e90de54dab9e36fda9beb25f306343a
hernanfloresramirez <hernan.flores@shipedge.com>
2025-03-30 22:48:49 -0400

    agregando environments al main.yml

--- src/app/core/_services/clients.service.ts
+++ src/app/core/_services/clients.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { ClientRegister } from '../_models/dto/users/clients/clientRegister.interface';
+import { ClienteSaveResponse } from '../_models/users/clients/clientesSaveResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,6 @@ export class ClientsService {
 
   getAllClients = (): Observable<any[]> => this.http.get<any[]>(`${this.#url}clientes`);
 
+  postSaveCliente = (employeeReg: ClientRegister): Observable<ClienteSaveResponse> => this.http.post<ClienteSaveResponse>(`${this.#url}clientes/register`, employeeReg);  
 
 }
