b4d5162b97774aa81d68afff943ac2900f98485a
hernanfloresramirez1987 <hernanfloresramirez1987@users.noreply.github.com>
2025-06-01 22:55:50 +0000

    Deploying to gh-pages from @ hernanfloresramirez1987/avitem-frontend@999055d6191cf8b803a51838e7b9c88d410a7417 🚀

---
+++ chunk-3Q6SWOLG.js
@@ -0,0 +1,1 @@
+import{Gb as r,Na as n,kb as a,lb as s}from"./chunk-4PTIY3UM.js";var o=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-warehouse"]],decls:2,vars:0,template:function(e,i){e&1&&(a(0,"p"),r(1,"warehouse works!"),s())},encapsulation:2})};export{o as default};
